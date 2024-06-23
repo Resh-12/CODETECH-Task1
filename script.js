18d04afcd17008c90f2b1729548ac68d330250fd
Reshma Bhat <135491157+Resh-12@users.noreply.github.com>
2024-06-23 15:53:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+ const searchbox=document.getElementById("search");
+ const listitem=document.getElementById("lists");
+ function addTask(){
+    if(searchbox.value===''){
+        alert("You must write something");
+    }else{
+        var item=document.createElement("li");
+        item.innerHTML=searchbox.value;
+        listitem.appendChild(item);
+            var span=document.createElement("span");
+            span.innerHTML="\u00d7";
+            item.appendChild(span);
+    }
+    searchbox.value="";
+    save();
+    
+ }
+ listitem.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("check");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+ },false);
+ function save(){
+    localStorage.setItem("data",listitem.innerHTML);
+ }
+ function display(){
+    listitem.innerHTML=localStorage.getItem("data");
+ }
+ display();
